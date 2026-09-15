d76ffa2b65ac461d3ae86f69c1cfe7e9fd0a35e6
Jonel <jonel.villanueva@tecsup.edu.pe>
2026-09-15 11:19:04 -0500

    feat(views): agregar nuevas páginas con el motor de plantillas pug

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// Write code here
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Configuración
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+
+// Rutas
+app.get('/', (req, res) => res.render('index'));
+app.get('/inicio', (req, res) => res.render('inicio'));
+app.get('/nosotros', (req, res) => res.render('nosotros'));
+app.get('/contactenos', (req, res) => res.render('contactenos'));
+app.get('/servicios', (req, res) => res.render('servicios'));
+app.get('/productos', (req, res) => res.render('productos'));
+
+// Servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
